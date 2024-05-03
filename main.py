d859a8beef7b481e39e742b148e45de731260c0f
strandafredde <strandafredde@gmail.com>
2024-05-04 00:04:11 +0200

    Fixed some issues

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import sys
 import pyautogui as pt
+import pygame
 from time import sleep
 
 class Clicker:
@@ -10,22 +12,38 @@ class Clicker:
         self.move_y = 75
     def nav_to_image(self):
         try:
-            position = pt.locateOnScreen(self.target_image, confidence=.8)
+            position = pt.locateOnScreen(self.target_image, confidence=.9)
             pt.moveTo(position[0] + self.move_x, position[1] + self.move_y, duration=self.speed)
             pt.click()
-
+            sleep(5)
+        except pt.FailSafeException:
+            print("FailSafe triggered, exiting program.")
+            exit(0)
         except Exception as e:
             print(f"Could not find target image: {e}")
             return 0
     
 if __name__ == "__main__":
     sleep(2)
-    clicker = Clicker("Images\slow-download.png", 0.01)
+    clicker = Clicker("Images\slow-download.png", 0.1)
     end = 0
-
+    pygame.init()
+    
     while True:
-       if clicker.nav_to_image() == 0:
-           end += 1
+        event = pygame.event.poll()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_e:
+                print("E pressed: Exiting program.")
+                pygame.quit()
+                sys.quit()
 
-           if end > 100:
-               break
+        if clicker.nav_to_image() == 0:
+            end += 1
+
+            if end > 200:
+                break
+        else:
+            end = 0
